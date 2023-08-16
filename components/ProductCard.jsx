47632b46442aaf72ada6eec8c91d28d807a718f7
Tamal Das <tamalcodes@gmail.com>
2023-08-17 03:43:52 +0530

    fix: minor changes to stylings

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -3,42 +3,38 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 const ProductCard = ({ data: { attributes: p, id } }) => {
-    return (
-        <Link
-            href={`/product/${p.slug}`}
-            className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
-        >
-            <Image
-                width={500}
-                height={500}
-                src={p.thumbnail.data.attributes.url}
-                alt={p.name}
-            />
-            <div className="p-4 text-black/[0.9]">
-                <h2 className="text-lg font-medium">{p.name}</h2>
-                <div className="flex items-center text-black/[0.5]">
-                    <p className="mr-2 text-lg font-semibold">
-                        &#8377;{p.price}
-                    </p>
+  return (
+    <Link
+      href={`/product/${p.slug}`}
+      className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer border border-gray-200  shadow-md 
+      hover:shadow-lg rounded-[10px] "
+    >
+      <Image
+        width={500}
+        height={500}
+        src={p.thumbnail.data.attributes.url}
+        alt={p.name}
+        className="w-full h-[300px] object-cover"
+      />
+      <div className="p-4 text-black/[0.9] md:h-[150px]">
+        <h2 className="text-xl font-semibold">{p.name}</h2>
+        <div className="flex items-center text-black/[0.5]">
+          <p className="mr-2 text-lg font-semibold">&#8377;{p.price}</p>
 
-                    {p.original_price && (
-                        <>
-                            <p className="text-base  font-medium line-through">
-                                &#8377;{p.original_price}
-                            </p>
-                            <p className="ml-auto text-base font-medium text-green-500">
-                                {getDiscountedPricePercentage(
-                                    p.original_price,
-                                    p.price
-                                )}
-                                % off
-                            </p>
-                        </>
-                    )}
-                </div>
-            </div>
-        </Link>
-    );
+          {p.original_price && (
+            <>
+              <p className="text-base  font-medium line-through">
+                &#8377;{p.original_price}
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                {getDiscountedPricePercentage(p.original_price, p.price)}% off
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
