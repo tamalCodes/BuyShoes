ccd26f8768892999b948b954159801ffc2730ad6
Tamal Das <tamalcodes@gmail.com>
2023-08-20 21:45:52 +0530

    fix: changes made to title

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Shoes</title>
+        <title>BuyShoes</title>
         <meta name="description" content="Grab some shoes." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
